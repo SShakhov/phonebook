9a5ae917862a7407fe06fcf048d1468b6443d404
SShakhov <sshakhov@lupus-pc.net>
2015-03-31 21:11:27 +0300

    Add basic interface

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -1,34 +1,112 @@
 package phonebook;
 
+import java.util.Scanner;
+
 public class Main
 {
+	public static Scanner s = new Scanner(System.in);
+	
 	public static void main(String[] args)
 	{
-		Book test = new Book();
+		Book book = new Book();
+		
+		while(true)
+		{
+			System.out.println("1: Create new entry");
+			System.out.println("2: Update existing entry");
+			System.out.println("3: Remove entry");
+			System.out.println("4: Print phonebook");
+			System.out.println("5: Exit");	
+
+			switch (s.nextInt()) 
+			{
+			case 1:
+				createNewEntry(book, s);
+				break;
+				
+			case 2:
+				updateEntry(book, s);
+				break;
+				
+			case 3:
+				removeEntry(book, s);
+				break;
+				
+			case 4:
+				s.nextLine();
+				System.out.println(book);
+				break;
+				
+			case 5:
+				s.close();
+				return;
+
+			default:
+				s.nextLine();
+				System.out.println("Please choose one of the listed options");
+				break;
+			}
+		}
+	}
+	
+	public static void createNewEntry(Book book, Scanner s)
+	{
+		s.nextLine();
+		
+		System.out.print("Enter a name: ");
+		String name = s.nextLine();
+		
+		System.out.print("Enter a phone number: ");
+		String number = s.nextLine();
+		
+		book.newEntry(name, number);
+	}
+	
+	public static void updateEntry(Book book, Scanner s)
+	{
+		s.nextLine();
+		
+		System.out.print("Enter a name for update: ");
+		String name = s.nextLine();
+		
 		try
 		{
-			test.newEntry("John Doe", "7915123456");
-			test.newEntry("Eugene Safronov", "79153296981");
-		
-		
-			System.out.println(test.getEntry("John Doe"));
-		
-			test.editEntry("John Doe", "7915654321");
-			System.out.println(test.getEntry("John Doe"));
-		
-			System.out.println("\n" + test + "\n");
-		
-			test.removeEntry("John Doe");
-		
-			System.out.println(test.getEntry("John Doe"));
+			System.out.println(book.getEntry(name));
+			System.out.print("Enter new phone number: ");
+			String newNumber = s.nextLine();
+			
+			book.editEntry(name, newNumber);
 		}
 		catch(EntryNotFoundException e)
 		{
-			System.out.println(e);
+			System.out.println(name + " not found");
 		}
-		catch(IllegalArgumentException e)
+	}
+	
+	public static void removeEntry(Book book, Scanner s)
+	{
+		s.nextLine();
+		
+		System.out.print("Enter a name for deletion: ");
+		String name = s.nextLine();
+		
+		try
 		{
-			System.out.println(e);
+			System.out.println(book.getEntry(name));
+			System.out.println("Do you want to remove this entry? (y/n)");
+			
+			String answer = s.nextLine();
+			answer = answer.trim();
+			answer = answer.toLowerCase();
+			
+			if(answer.equals("y"))
+				book.removeEntry(name);
+			
+			return;
+		}
+		catch(EntryNotFoundException e)
+		{
+			System.out.println(name + " not found");
 		}
 	}
 }
