3040be80f6c034690eba78cdc5220b48f668853e
SShakhov <sshakhov@lupus-pc.net>
2015-03-31 21:27:00 +0300

    Move try/catch out of functions

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -17,34 +17,45 @@ public class Main
 			System.out.println("3: Remove entry");
 			System.out.println("4: Print phonebook");
 			System.out.println("5: Exit");	
-
-			switch (s.nextInt()) 
+			
+			try
 			{
-			case 1:
-				createNewEntry(book, s);
-				break;
-				
-			case 2:
-				updateEntry(book, s);
-				break;
-				
-			case 3:
-				removeEntry(book, s);
-				break;
-				
-			case 4:
-				s.nextLine();
-				System.out.println(book);
-				break;
-				
-			case 5:
-				s.close();
-				return;
-
-			default:
-				s.nextLine();
-				System.out.println("Please choose one of the listed options");
-				break;
+				switch (s.nextInt()) 
+				{
+				case 1:
+					createNewEntry(book, s);
+					break;
+					
+				case 2:
+					updateEntry(book, s);
+					break;
+					
+				case 3:
+					removeEntry(book, s);
+					break;
+					
+				case 4:
+					s.nextLine();
+					System.out.println(book);
+					break;
+					
+				case 5:
+					s.close();
+					return;
+					
+				default:
+					s.nextLine();
+					System.out.println("Please choose one of the listed options");
+					break;
+				}
+			}
+			catch(EntryNotFoundException e)
+			{
+				System.out.println(e.getMessage());
+			}
+			catch(IllegalArgumentException e)
+			{
+				System.out.println(e.getMessage());
 			}
 		}
 	}
@@ -62,51 +73,35 @@ public class Main
 		book.newEntry(name, number);
 	}
 	
-	public static void updateEntry(Book book, Scanner s)
+	public static void updateEntry(Book book, Scanner s) throws EntryNotFoundException
 	{
 		s.nextLine();
 		
 		System.out.print("Enter a name for update: ");
 		String name = s.nextLine();
 		
-		try
-		{
-			System.out.println(book.getEntry(name));
-			System.out.print("Enter new phone number: ");
-			String newNumber = s.nextLine();
+		System.out.println(book.getEntry(name));
+		System.out.print("Enter new phone number: ");
+		String newNumber = s.nextLine();
 			
-			book.editEntry(name, newNumber);
-		}
-		catch(EntryNotFoundException e)
-		{
-			System.out.println(name + " not found");
-		}
+		book.editEntry(name, newNumber);
 	}
 	
-	public static void removeEntry(Book book, Scanner s)
+	public static void removeEntry(Book book, Scanner s) throws EntryNotFoundException
 	{
 		s.nextLine();
 		
 		System.out.print("Enter a name for deletion: ");
 		String name = s.nextLine();
 		
-		try
-		{
-			System.out.println(book.getEntry(name));
-			System.out.println("Do you want to remove this entry? (y/n)");
+		System.out.println(book.getEntry(name));
+		System.out.println("Do you want to remove this entry? (y/n)");
 			
-			String answer = s.nextLine();
-			answer = answer.trim();
-			answer = answer.toLowerCase();
+		String answer = s.nextLine();
+		answer = answer.trim();
+		answer = answer.toLowerCase();
 			
-			if(answer.equals("y"))
-				book.removeEntry(name);
-			
-			return;
-		}
-		catch(EntryNotFoundException e)
-		{
-			System.out.println(name + " not found");
-		}
+		if(answer.equals("y"))
+			book.removeEntry(name);
 	}
 }
